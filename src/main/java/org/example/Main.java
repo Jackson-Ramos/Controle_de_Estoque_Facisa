950c4cd0d72ef770c39f1074bbeb2ec3a247d940
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-04-02 19:25:16 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		int ops = 10;
+		
+		while (ops != 0) {
+			String ops2 = JOptionPane.showInputDialog(null, """
+					[1] FLUSSO DD CAIXA
+					[2] CONTROLE DE ESTOQUE
+					""");
+			switch (Integer.parseInt(ops2)){
+				case 1:{
+				
+				}
+				case 2:{
+				
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/org/example/entities/Produto.java
@@ -0,0 +1,59 @@
+package org.example.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Produto {
+	
+	private String nome;
+	private Long codigo;
+	private Double preco;
+	private static List<Produto> produtoList = new ArrayList<>();
+	
+	public Produto(String nome, Long codigo, Double preco) {
+		this.nome = nome;
+		this.codigo = codigo;
+		this.preco = preco;
+	}
+	
+	public Produto() {
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	
+//	public void setCodigo(Long codigo) {
+//		this.codigo = codigo;
+//	}
+	
+	public Double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Produto produto = (Produto) o;
+		return Objects.equals(codigo, produto.codigo);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+}
